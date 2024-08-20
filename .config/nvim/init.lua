93990a258271df801139fc2c12ec097bc71c276b
Harim Seong <halim9512@gmail.com>
2024-08-20 11:44:55 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -415,7 +415,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', } 
+local servers = { 'clangd', 'rust_analyzer' } 
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
